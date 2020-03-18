c5976f8091ed049bec75b4d1eeed121394758eda
Adrian Romański <muszynianka1337@gmail.com>
2020-03-18 15:09:28 +0100

    Splitting into Modules

---
+++ rest-model/src/test/java/adrianromanski/restmodel/RestModelApplicationTests.java
@@ -0,0 +1,13 @@
+package adrianromanski.restmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
